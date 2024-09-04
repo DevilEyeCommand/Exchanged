fe4a7753d7a7f34c8b372f2f8dc842f0e53edf08
ChaimongkhonS <chaimongkhon.s2002@gmail.com>
2024-09-04 17:45:45 +0700

    update-CardExchange

---
+++ components/Use/UseCardExchage.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="flex justify-center items-center gap-4 py-4">
+    <div class="card bg-neutral text-neutral-content w-96">
+      <div class="card-body items-center text-center">
+        <select class="select w-full max-w-xs select-error bg-neutral">
+          <option disabled selected>เลือกประเทศ</option>
+        </select>
+        <label class="form-control w-full max-w-xs">
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input input-bordered input-primary w-full max-w-xs bg-neutral"
+          />
+          <div class="label">
+            <span class="label-text-alt text-base-300">1 USD = 34.220 THB</span>
+          </div>
+        </label>
+      </div>
+    </div>
+    <button class="btn">
+      <Icon name="system-uicons:reverse" size="50" />
+    </button>
+    <div class="card bg-neutral text-neutral-content w-96">
+      <div class="card-body items-center text-center">
+        <select class="select w-full max-w-xs select-error bg-neutral">
+          <option disabled selected>เลือกประเทศ</option>
+        </select>
+        <label class="form-control w-full max-w-xs">
+          <input
+            type="text"
+            placeholder="Type here"
+            class="input input-bordered input-primary w-full max-w-xs bg-neutral"
+          />
+          <div class="label">
+            <span class="label-text-alt text-base-300">1 USD = 34.220 THB</span>
+          </div>
+        </label>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup></script>
