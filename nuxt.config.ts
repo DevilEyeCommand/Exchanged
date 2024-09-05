77824ad47338f1ca2886e6752f222a78e9a938d7
ChaimongkhonS <chaimongkhon.s2002@gmail.com>
2024-09-05 14:51:13 +0700

    npm i Echart

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,43 +5,44 @@ export default defineNuxtConfig({
  css: ["@/assets/css/main.css"],
 
  modules: [
-					"@nuxtjs/tailwindcss",
-					"@nuxtjs/google-fonts",
-					"@ant-design-vue/nuxt",
-					"@nuxtjs/color-mode",
-					"@nuxt/icon",
+	 "@nuxtjs/tailwindcss",
+	 "@nuxtjs/google-fonts",
+	 "@ant-design-vue/nuxt",
+	 "@nuxtjs/color-mode",
+	 "@nuxt/icon",
+	 "nuxt-echarts",
 	],
 
  compilerOptions: {
-					resolveJsonModule: true,
-	},
+																				resolveJsonModule: true,
+				},
 
  googleFonts: {
-					families: {
-									Kanit: [100, 200, 300, 400, 500, 600, 700, 800, 900],
-					},
-	},
+																				families: {
+																																				Kanit: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+																				},
+				},
 
  tailwindcss: {
-					config: {
-									theme: {
-													extend: {
-																	colors: {
-																					primary: "#E7A339",
-																	},
-													},
-									},
-					},
-	},
+																				config: {
+																																				theme: {
+																																																				extend: {
+																																																																				colors: {
+																																																																																				primary: "#E7A339",
+																																																																				},
+																																																				},
+																																				},
+																				},
+				},
 
  colorMode: {
-					preference: "light",
-					dataValue: "theme",
-	},
+																				preference: "light",
+																				dataValue: "theme",
+				},
 
  server: {
-					host: "0.0.0.0",
-	},
+																				host: "0.0.0.0",
+				},
 
  compatibilityDate: "2024-08-31",
 });
